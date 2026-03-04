07f5336e489d8e4900e2cd777d976134c190ef50
cdbope <cdbope@users.noreply.github.com>
2026-03-04 13:21:32 +0100

    Remove legacy nanoDx folder and update Rmd report

--- nanoDx/run_local_gpu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-snakemake --cores all --resources pdfReport=1 --resources gpu=1 --use-conda $*
--- nanoDx/run_slurm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-snakemake --jobs 500 \
-          --use-conda \
-          --default-resources "mem_mb=8192" "runtime=240" \
-          --resources pdfReport=1 \
-          --slurm \
-          $*
